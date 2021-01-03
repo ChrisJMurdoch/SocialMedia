4608bc878214e485387fcfe1d49cc9230d845293
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2021-01-03 04:37:20 +0000

    Implemented context-destroyed listener to avoid driver leaks

---
+++ src/main/java/listeners/Lifetime.java
@@ -0,0 +1,41 @@
+package listeners;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import persistence.Database;
+
+/** Simple listener class to act upon server startup */
+public class Lifetime implements ServletContextListener {
+	
+    public Lifetime() {
+        System.out.println("\n=== Server starting ===");
+        Database.connect();
+        System.out.println("=== Server started ===\n");
+    }
+    
+    public void contextDestroyed(ServletContextEvent arg0)  {
+    	
+    	System.out.println("\n=== Server closing ===");
+    	
+    	// Close JDBC drivers
+    	System.out.println("Deregistering drivers...");
+    	Enumeration<Driver> drivers = DriverManager.getDrivers();
+    	while (drivers.hasMoreElements()) {
+    		try {
+				DriverManager.deregisterDriver( (Driver)drivers.nextElement() );
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+    	}
+    	System.out.println("Drivers deregistered.");
+    	
+        System.out.println("=== Server closed ===\n");
+    }
+    
+    public void contextInitialized(ServletContextEvent arg0)  {}
+}
